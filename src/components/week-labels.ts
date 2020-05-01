66fe61184fc86018accb7308bdf6be811d678f4c
Mohammad Honravar <honarvar.info@gmail.com>
2020-05-01 18:44:55 +0430

    fix(wek-labels): style & strucure issues

--- src/components/week-labels.ts
+++ src/components/week-labels.ts
@@ -1,6 +1,6 @@
 import { html, css, customElement, TemplateResult, property } from 'lit-element';
+
 import { BaseElement } from '../base-element';
-import { WeekDayList as WeekDays } from '../data/gregorian';
 
 interface WeekLabelInterface {
   name: string;
@@ -9,19 +9,17 @@ interface WeekLabelInterface {
 
 @customElement('week-labels')
 export class WeekLabelList extends BaseElement {
+  @property({ type: Array })
+  weekLabelList: WeekLabelInterface[] | undefined;
 
-  @property({ type: Boolean })
+  @property({ type: Boolean, attribute: 'short-name' })
   useShortName: boolean = false;
 
   static styles = css`
-
-    .week-labels-row {
+    :host {
       display: flex;
       padding: 0 8px;
-      margin: 6px 0;
-    }
-
-    .week-labels-row {
+      margin-top: 6px;
       margin-bottom: 8px;
       font-weight: 500;
       font-size: 0.85em;
@@ -29,7 +27,7 @@ export class WeekLabelList extends BaseElement {
       color: rgba(0, 0, 0, 0.38);
     }
 
-    .week-labels-row .calendar-day{
+    .calendar-day{
       /* background-color: tomato; */
       /* margin: 2px; */
       font-weight: 500;
@@ -37,7 +35,7 @@ export class WeekLabelList extends BaseElement {
       position: relative;
     }
 
-    .week-labels-row .calendar-day::after{
+    .calendar-day::after{
       content: "";
       float:left;
       display: block;
@@ -48,20 +46,18 @@ export class WeekLabelList extends BaseElement {
 
   protected shouldUpdate(): boolean {
     console.log(this.useShortName);
-    return Array.isArray(WeekDays) && WeekDays.length > 0;
+    return Array.isArray(this.weekLabelList) && this.weekLabelList.length > 0;
   }
 
   protected render(): TemplateResult {
     return html`
-    <div class="week-labels-row">
-    ${(WeekDays as WeekLabelInterface[]).map(({ shortName, name }: WeekLabelInterface) => {
-      return html`
-            <div class="calendar-day">
-              <div class="calendar-day-data">${this.useShortName ? shortName : name}</div>
-            </div>
-          `
-    })}
-      </div>
-    `;
+      ${(this.weekLabelList as WeekLabelInterface[]).map(({ shortName, name }: WeekLabelInterface) => {
+        return html`
+          <div class="calendar-day">
+            <div class="calendar-day-data">${this.useShortName ? shortName : name}</div>
+          </div>
+        `
+      })
+    }`;
   }
 }
